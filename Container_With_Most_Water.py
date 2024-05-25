1d2885f0a3dfc16019f52dcfed10c4a7e26ae711
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-05-25 23:52:42 +0200

    Update Container_With_Most_Water.py
    Final version of my inefficient solution

--- Container_With_Most_Water.py
+++ Container_With_Most_Water.py
@@ -60,6 +60,54 @@ class Solution(object):
             heightLine -=1
         return maxArea
 
+# class Solution(object):
+#     def maxArea(self, height):
+#         """
+#         :type height: List[int]
+#         :rtype: int
+#         """
+#         # heightLine is a straightline that will descend from the heighest point/s until it meets the x-axis
+#         # Think of it as a horizontal line/window that senses the bars it touches
+#         heightLine = max(height)
+#         maxArea = 0
+#         i = 0
+#         # At the current height I check which bars exist 
+#         while heightLine > 0:
+#             elems = [h for h in height if h >= heightLine]
+            
+#             if len(elems) == 1:
+#                 x = 0
+#                 Area = abs(x * heightLine)
+#             elif not elems:
+#                 x = 0
+#                 Area = abs(x * heightLine)
+#             else:
+#                 leftmost = float('inf')
+#                 rightmost = -1
+
+#                 for i, val in enumerate(height):
+#                     if val >= heightLine and i < leftmost:
+#                         leftmost = i
+#                     if val >= heightLine and i > rightmost:
+#                         rightmost = i
+
+#                 x = abs(rightmost - leftmost) if leftmost != float('inf') and rightmost != -1 else 0
+
+
+#                 # Here i find the max absolute value of the volume for the current height
+#                 Area = abs(x * heightLine)
+
+#                 #if the current area is greater than maxArea, then i make it the maxArea
+#             maxArea = max(maxArea,Area)
+                
+#                 #print("Area:{}  maxArea:{}  elems:{}  heightLine:{}  x:{}".format(Area, maxArea, elems, heightLine, x))
+#                 # print("min:{} max:{}".format(height.index(min(elems)), height.index(max(elems)) ))
+#             heightLine -=1
+#         return maxArea
+
+
+
+
 
 
  
